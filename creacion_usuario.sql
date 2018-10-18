b4d9735f6bd621174ece7d948e68ce58302e3f36
Lab <lab@correo.com>
2018-10-18 08:53:47 -0600

    Agregar cosa

--- creacion_usuario.sql
+++ creacion_usuario.sql
@@ -1,4 +1,5 @@
-CREATE USER hospital_admin IDENTIFIED BY hospital_admin;
+alter session set "_ORACLE_SCRIPT"=true;  
+CREATE USER hospital_admin IDENTIFIED BY hospital_admin123;
 GRANT CONNECT TO hospital_admin;
 GRANT RESOURCE TO hospital_admin;
 GRANT DBA TO hospital_admin;
